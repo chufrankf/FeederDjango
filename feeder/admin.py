d6be7e627f3bc024b3bfc10120d46b9cddbe2ab1
chufrankf <chufrankf@gmail.com>
2017-04-13 23:54:52 -0700

    First commit

---
+++ feeder/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+# This registration is for djangogirls
+admin.site.register(Post)
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/frankchu/Documents/Django/DjangoEnv/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
